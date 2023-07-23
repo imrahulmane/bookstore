5af491133aa40bd333175bec475ac5eebf05b36e
imrahulmane <maneraul8298@gmail.com>
2023-07-23 20:19:19 +0530

    ENV file added

--- app/util/DatabaseUtil.php
+++ app/util/DatabaseUtil.php
@@ -10,7 +10,7 @@ class DatabaseUtil
     private $dbName="bookstore";
 
     public function getConnection($collection_name) {
-        $client = new MongoDB\Client('mongodb://172.18.0.11:27017');
+        $client = new MongoDB\Client(getenv("DB_URL"));
         $db = $client->selectDatabase($this->dbName);
         $options = ["typeMap" => ['root' => 'array', 'document' => 'array']];
         $colln = $db->selectCollection($collection_name,$options);
